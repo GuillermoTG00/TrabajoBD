d421290bf8b1c8f8aa57e7b51944d16f678dc1ca
Cristian Mejia <crmejiam120@gmail.com>
2022-06-28 04:01:52 -0500

    update busquedas.php

--- busquedas/busquedas.php
+++ busquedas/busquedas.php
@@ -69,7 +69,8 @@
                     "CONSULTA 1" => "SELECT cedula, nombre FROM alquiler
                                     JOIN cliente ON cedula = cedulacliente
                                     GROUP BY cedula, nombre
-                                    ORDER BY sum(valoralquiler) DESC",
+                                    ORDER BY sum(valoralquiler) DESC
+                                    LIMIT 2",
                     "CONSULTA 2" => "SELECT cedula, nombre, count(*) AS paquetes, sum(peso) sumapeso, extract(month FROM fecha_envio) AS mes
                                     FROM cliente JOIN paquete ON cedula = cedula_del_receptor
                                     GROUP BY cedula, nombre, extract(month FROM fecha_envio)
